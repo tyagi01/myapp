673a972dcfbf4fa90c7a1a9e997a22f3c6b6c710
tyagi01 <vratikatyagi01@gmail.com>
2022-06-07 11:55:46 +0530

    post request

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var abcRouter = require('./routes/abc');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/abc', abcRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
